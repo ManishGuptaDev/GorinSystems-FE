9e24c6c22ba03133866cca16790ca1531a48e2b4
Manish Gupta <manish.gta1@gmail.com>
2024-09-22 20:55:41 +0530

    types

--- src/components/organisms/LandingPage/index.tsx
+++ src/components/organisms/LandingPage/index.tsx
@@ -1,13 +1,64 @@
-import Banner from "@/components/molecules/Banner"
-import { FC } from "react"
+import Banner from "@/components/molecules/Banner";
+import { FC } from "react";
+import styles from "./LandingPage.module.css";
+import Retail from "@/assets/icons/Retail";
+import Family from "@/assets/icons/Family";
+import Building from "@/assets/icons/Building";
+import WareHouse from "@/assets/icons/WareHouse";
+import Hotel from "@/assets/icons/Hotel";
+import Land from "@/assets/icons/Land";
 
-const LandingPage : FC = () => {
-    return (
-        <div>
-            <Banner />
-            landing page
+const itmes = [
+  {
+    name: "Retail",
+    icon: <Retail />,
+  },
+  {
+    name: "Multi-family",
+    icon: <Family />,
+  },
+  {
+    name: "Office",
+    icon: <Building />,
+  },
+  {
+    name: "Industrial",
+    icon: <WareHouse />,
+  },
+  {
+    name: "Hotel",
+    icon: <Hotel />,
+  },
+  {
+    name: "Land",
+    icon: <Land />,
+  },
+];
+
+const LandingPage: FC = () => {
+  return (
+    <div>
+      <Banner />
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <p className={styles.heading}>Create OMs and Ads For</p>
+          <p className={styles.subheading}>
+            Various Real Estate Property Types
+          </p>
         </div>
-    )
-}
+        <div className={styles.bottom}>
+          {itmes.map((item, index) => {
+            return (
+              <div className={styles.item} key={index}>
+                {item.icon}
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
